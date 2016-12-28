2d48071973d84be650c8d3599039cad4e91fb2e9
sdrapkin <sdrapkin@sdprime.com>
2016-12-27 19:08:54 -0500

    QueryBuilder, SequentialGuid.

---
+++ Helpers/QB.cs
@@ -0,0 +1,227 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace SecurityDriven.TinyORM.Helpers
+{
+	using Utils;
+	using Helpers;
+
+	using QueryInfoTuple = Tuple<string, Dictionary<string, object>>;
+
+	/// <summary>QueryBuilder.</summary>
+	public static class QB
+	{
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static string AsSqlName<T>(this T obj) => typeof(T).Name.AsSqlName();
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static string AsSqlName(this string str) => "[" + str.Replace("]", "]]") + "]";
+
+		const int AVERAGE_PROPERTY_LENGTH = 14 + 2; // assumed average property length, plus 2 characters for "@" and ("," or "=").
+
+		#region Insert<T>()
+
+		public static QueryInfoTuple Insert<T>(T obj, Predicate<string> propFilter = null, string tableName = null) where T : class
+		{
+			if (tableName == null) tableName = obj.AsSqlName();
+			var dict = ObjectFactory.ObjectToDictionary(obj);
+			var dictCount = dict.Count;
+			var dictNew = new Dictionary<string, object>(dictCount, Util.FastStringComparer.Instance);
+
+			int i = 0;
+			string currentKey, paramName;
+
+			var sb = new StringBuilder("INSERT ", AVERAGE_PROPERTY_LENGTH * dictCount * 2 + 25).Append(tableName).Append(" (");
+			var sbParams = new StringBuilder(AVERAGE_PROPERTY_LENGTH * dictCount);
+			foreach (var kvp in dict)
+			{
+				currentKey = kvp.Key;
+				if (propFilter != null && !propFilter(currentKey))
+					continue;
+
+				if (i++ != 0)
+				{
+					sb.Append(',');
+					sbParams.Append(',');
+				}
+				sb.Append('[').Append(currentKey).Append(']');
+				paramName = "@@" + currentKey;
+				sbParams.Append(paramName);
+				dictNew.Add(paramName, kvp.Value);
+			}//foreach
+
+			sb.Append(") VALUES (").Append(sbParams.ToString()).Append(')');
+			var result = new QueryInfoTuple(sb.ToString(), dictNew);
+			return result;
+		}// Insert<T>()
+		#endregion
+
+		#region Update<T, TParamType>()
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static QueryInfoTuple Update<T>(T obj, string whereSql = null, Predicate<string> propFilter = null, string tableName = null, Dictionary<string, object> dict = null) where T : class
+		{
+			return Update<T, string>(obj: obj, whereSql: whereSql, whereParam: null, propFilter: propFilter, tableName: tableName, dict: dict);
+		}// Update<T>
+
+		public static QueryInfoTuple Update<T, TParamType>(T obj, string whereSql = null, TParamType whereParam = null, Predicate<string> propFilter = null, string tableName = null, Dictionary<string, object> dict = null) where T : class where TParamType : class
+		{
+			if (dict == null) dict = ObjectFactory.ObjectToDictionary(obj);
+			QueryInfoTuple queryInfoTuple = UpdateRaw<T>(obj, propFilter, tableName, dict);
+			var paramDictAlias = queryInfoTuple.Item2;
+			var whereParamMap = default(Dictionary<string, object>);
+
+			if (string.IsNullOrEmpty(whereSql))
+			{
+				object id;
+				if (!dict.TryGetValue("Id", out id)) throw new ArgumentException(@"""whereSql"" is empty and object does not contain ""Id"" property.");
+
+				whereSql = "Id=@w@Id";
+				paramDictAlias.Add("@w@Id", id);
+			}
+			else if (whereParam != null)
+			{
+				whereParamMap = whereParam as Dictionary<string, object> ?? ObjectFactory.ObjectToDictionary(whereParam, parameterize: true);
+				foreach (var kvp in whereParamMap)
+					paramDictAlias.Add(kvp.Key, kvp.Value);
+			}
+			whereSql = queryInfoTuple.Item1 + " WHERE " + whereSql;
+			queryInfoTuple = new QueryInfoTuple(whereSql, paramDictAlias);
+			return queryInfoTuple;
+		}// Update<T, TParamType>()
+		#endregion
+
+		#region UpdateRaw<T>()
+		internal static QueryInfoTuple UpdateRaw<T>(T obj, Predicate<string> propFilter = null, string tableName = null, Dictionary<string, object> dict = null) where T : class
+		{
+			if (tableName == null) tableName = obj.AsSqlName();
+			if (dict == null) dict = ObjectFactory.ObjectToDictionary(obj);
+			var dictCount = dict.Count;
+			var dictNew = new Dictionary<string, object>(dictCount, Util.FastStringComparer.Instance);
+
+			int i = 0;
+			string currentKey, paramName;
+
+			var sb = new StringBuilder("UPDATE ", AVERAGE_PROPERTY_LENGTH * dictCount * 2 + 12).Append(tableName).Append(" SET ");
+			foreach (var kvp in dict)
+			{
+				currentKey = kvp.Key;
+				if (propFilter != null && !propFilter(currentKey))
+					continue;
+
+				if (i++ != 0)
+				{
+					sb.Append(',');
+				}
+				paramName = "@@" + currentKey;
+				sb.Append('[').Append(currentKey).Append("]=").Append(paramName);
+				dictNew.Add(paramName, kvp.Value);
+			}//foreach
+
+			if (i == 0) throw new ArgumentException("propFilter predicate is false for all property names.", "propFilter");
+			var result = new QueryInfoTuple(sb.ToString(), dictNew);
+			return result;
+		}// UpdateRaw<T>()
+		#endregion
+
+		#region Delete<TParamType>()
+		public static QueryInfoTuple Delete<TParamType>(string tableName = null, string whereSql = null, TParamType whereParam = null) where TParamType : class
+		{
+			if (string.IsNullOrEmpty(tableName)) throw new ArgumentNullException("tableName");
+			if (string.IsNullOrEmpty(whereSql)) throw new ArgumentNullException("whereSql");
+
+			string sql = "DELETE FROM " + tableName + " WHERE " + whereSql;
+			var whereParamMap = default(Dictionary<string, object>);
+
+			if (whereParam != null) whereParamMap = whereParam as Dictionary<string, object> ?? ObjectFactory.ObjectToDictionary(whereParam, parameterize: true);
+			var result = new QueryInfoTuple(sql, whereParamMap);
+			return result;
+		}// Delete<TParamType>()
+		#endregion
+
+		#region Upsert<T>
+		public static QueryInfoTuple Upsert<T>(T obj, string tableName = null, Predicate<string> insertPropFilter = null, Predicate<string> updatePropFilter = null, string mergeOnSql = null) where T : class
+		{
+			if (tableName == null) tableName = obj.AsSqlName();
+			if (string.IsNullOrEmpty(mergeOnSql)) mergeOnSql = "S.Id=T.Id";
+			var dict = ObjectFactory.ObjectToDictionary(obj);
+			var dictCount = dict.Count;
+			var dictParams = new Dictionary<string, object>(dictCount, Util.FastStringComparer.Instance);
+
+			string currentKey, currentKeyBracketed, paramName;
+
+			var sbSql = new StringBuilder(";WITH S(");
+			var sbParams = new StringBuilder();
+			var sbInsertColumns = new StringBuilder();
+			var sbInsertValues = new StringBuilder();
+			var sbUpdateSql = new StringBuilder();
+
+			int i = 0, insertColumnCount = 0, updateColumnCount = 0;
+
+			foreach (var kvp in dict)
+			{
+				currentKey = kvp.Key;
+				currentKeyBracketed = "[" + currentKey + "]";
+				if (i++ != 0)
+				{
+					sbSql.Append(',');
+					sbParams.Append(',');
+				}
+
+				sbSql.Append(currentKeyBracketed);
+				paramName = "@@" + currentKey;
+				sbParams.Append(paramName);
+				dictParams.Add(paramName, kvp.Value);
+
+				if (insertPropFilter == null || insertPropFilter(currentKey))
+				{
+					if (insertColumnCount++ > 0)
+					{
+						sbInsertColumns.Append(',');
+						sbInsertValues.Append(',');
+					}
+					sbInsertColumns.Append(currentKeyBracketed);
+					sbInsertValues.Append(paramName);
+				}
+
+				if (updatePropFilter == null || updatePropFilter(currentKey))
+				{
+					if (updateColumnCount++ > 0)
+					{
+						sbUpdateSql.Append(',');
+					}
+					sbUpdateSql.Append(currentKeyBracketed).Append('=').Append(paramName);
+				}
+			}//foreach
+
+			sbSql
+				.Append(") AS (SELECT ")
+				.Append(sbParams.ToString())
+				.Append(")\nMERGE ")
+				.Append(tableName)
+				.Append(" T USING S ON ")
+				.Append(mergeOnSql)
+				.Append("\nWHEN NOT MATCHED THEN\n\tINSERT (")
+				.Append(sbInsertColumns.ToString()).Append(") VALUES (")
+				.Append(sbInsertValues);
+
+			if (updateColumnCount > 0)
+			{
+				sbSql
+					.Append(")\nWHEN MATCHED THEN\n\tUPDATE SET ")
+					.Append(sbUpdateSql.ToString());
+			}
+			else
+			{
+				sbSql.Append(")\n");
+			}
+
+			var result = new QueryInfoTuple(sbSql.ToString(), dictParams);
+			return result;
+		}//Upsert()
+		#endregion
+
+	}//class QB
+}//ns
---
+++ Helpers/SequentialGuid.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace SecurityDriven.TinyORM.Helpers
+{
+	public static class SequentialGuid
+	{
+		public static Guid NewSequentialGuid() => GuidStruct.NewSequentialGuid();
+
+		#region LongStruct
+		[StructLayout(LayoutKind.Explicit, Pack = 1)]
+		struct LongStruct
+		{
+			[FieldOffset(0)]
+			public long Value;
+
+			[FieldOffset(0)]
+			public byte B0;
+			[FieldOffset(1)]
+			public byte B1;
+			[FieldOffset(2)]
+			public byte B2;
+			[FieldOffset(3)]
+			public byte B3;
+			[FieldOffset(4)]
+			public byte B4;
+			[FieldOffset(5)]
+			public byte B5;
+			[FieldOffset(6)]
+			public byte B6;
+			[FieldOffset(7)]
+			public byte B7;
+		}// struct LongStruct
+		#endregion
+
+		#region GuidStruct
+		[StructLayout(LayoutKind.Explicit, Pack = 1)]
+		struct GuidStruct
+		{
+			[FieldOffset(0)]
+			public Guid Value;
+
+			[FieldOffset(0)]
+			public int IntValue0123;
+			[FieldOffset(4)]
+			public short ShortValue45;
+			[FieldOffset(10)]
+			public short ShortValueAB;
+
+			[FieldOffset(0)]
+			public byte B0;
+			[FieldOffset(1)]
+			public byte B1;
+			[FieldOffset(2)]
+			public byte B2;
+			[FieldOffset(3)]
+			public byte B3;
+			[FieldOffset(4)]
+			public byte B4;
+			[FieldOffset(5)]
+			public byte B5;
+
+			[FieldOffset(6)] // biased for GUID_v4: always 4x
+			public byte B6;
+			[FieldOffset(7)]
+			public byte B7;
+			[FieldOffset(8)] // biased in GUID_v4: [8x, 9x, Ax, Bx]
+			public byte B8;
+			[FieldOffset(9)]
+			public byte B9;
+
+			[FieldOffset(10)]
+			public byte BA;
+			[FieldOffset(11)]
+			public byte BB;
+
+			[FieldOffset(12)]
+			public byte BC;
+			[FieldOffset(13)]
+			public byte BD;
+			[FieldOffset(14)]
+			public byte BE;
+			[FieldOffset(15)]
+			public byte BF;
+
+			public static Guid NewSequentialGuid()
+			{
+				var guidStruct = new GuidStruct { Value = Guid.NewGuid() };
+				var uuidStruct = new GuidStruct { Value = GetSequentialUuid() };
+
+				return new Guid(
+					a: guidStruct.IntValue0123,
+					b: guidStruct.ShortValue45,
+
+					c: guidStruct.ShortValueAB,
+
+					d: uuidStruct.B1,
+					e: uuidStruct.B0,
+
+					f: uuidStruct.B7,
+					g: uuidStruct.B6,
+					h: uuidStruct.B5,
+					i: uuidStruct.B4,
+					j: uuidStruct.B3,
+					k: uuidStruct.B2);
+			}// NewSequentialGuid()
+
+			[DllImport("rpcrt4.dll", SetLastError = true)]
+			static extern int UuidCreateSequential(out Guid guid);
+
+			static Guid GetSequentialUuid()
+			{
+				const int RPC_S_OK = 0; Guid g;
+				if (UuidCreateSequential(out g) == RPC_S_OK) return g;
+				throw new Exception("UuidCreateSequential did not return RPC_S_OK.");
+			}//GetSequentialUuid()
+		}// struct GuidStruct
+		#endregion
+
+		// https://tools.ietf.org/html/rfc4122 UUID zero-tick starts on 1582-October-15 (Gregorian reform to the Christian calendar)
+		static readonly long UuidStartTicks = new DateTime(1582, 10, 15, 0, 0, 0, DateTimeKind.Utc).Ticks;
+		public static DateTime ExtractDateTimeUtc(this Guid g)
+		{
+			var guidStruct = new GuidStruct { Value = g };
+			var longStruct = new LongStruct
+			{
+				B4 = guidStruct.BD,
+				B5 = guidStruct.BC,
+				B6 = guidStruct.BB,
+				B7 = (byte)(guidStruct.BA & 0x0F)
+			};
+			return new DateTime(longStruct.Value + UuidStartTicks, DateTimeKind.Utc);
+		}// ExtractDateTimeUtc()
+	}//class SequentialGuid
+}//ns

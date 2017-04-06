4c2116e0e75f7edf1883470527a6164ffa8c3525
sdrapkin <sdrapkin@sdprime.com>
2017-04-06 17:35:09 -0400

    CLSCompliant=true

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -33,3 +33,4 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.6.*")]
+[assembly: System.CLSCompliant(true)]

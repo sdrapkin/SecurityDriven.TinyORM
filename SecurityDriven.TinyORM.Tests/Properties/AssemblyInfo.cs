4a90ebc2e36db18bdf6a10b18114c8f8f62fb2b3
sdrapkin <sdrapkin@sdprime.com>
2017-04-19 21:43:16 -0400

    Tests project added.

---
+++ SecurityDriven.TinyORM.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SecurityDriven.TinyORM.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SecurityDriven.TinyORM.Tests")]
+[assembly: AssemblyCopyright("Copyright © 2017 Stan Drapkin")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("79bfb495-07b1-4ac1-8c0e-0efa8633d6a7")]
+
+[assembly: AssemblyVersion("1.0.*")]
---
+++ SecurityDriven.TinyORM.Tests/Sanity.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace SecurityDriven.TinyORM.Tests
+{
+	using SecurityDriven.TinyORM;
+
+	[TestClass]
+	public class Sanity
+	{
+		static readonly string connString = System.Configuration.ConfigurationManager.ConnectionStrings["Test"].ConnectionString;
+		static readonly Version tinyormVersion = typeof(DbContext).Assembly.GetName().Version;
+
+		[ClassInitialize]
+		public static void Initialize(TestContext testContext)
+		{
+		}
+
+		[ClassCleanup]
+		public static void Cleanup()
+		{
+		}
+
+		[TestMethod]
+		public async Task ConnectionTest()
+		{
+			var db = DbContext.CreateDbContext(connString);
+			var results = await db.QueryAsync("select [Answer] = 1 + 2, [Guid] = NEWID() UNION ALL SELECT 5, 0x");
+			Assert.IsTrue(results.Count == 2);
+
+			var firstRow = results[0];
+			int expectedAnswer = 3;
+			Assert.IsTrue(firstRow.Answer == expectedAnswer);
+			Assert.IsTrue(firstRow["Answer"] == expectedAnswer);
+			Assert.IsTrue(firstRow[0] == expectedAnswer);
+			Assert.IsTrue(firstRow.NonExistentColumn is FieldNotFound);
+			Assert.IsTrue((firstRow as RowStore).Schema.FieldMap.Count == 2);
+
+			var secondRow = results[1];
+			expectedAnswer = 5;
+			Assert.IsTrue(secondRow.Answer == expectedAnswer);
+			((Guid)firstRow.Guid).GetHashCode();
+			Assert.IsTrue((Guid)secondRow.Guid != (Guid)firstRow.Guid);
+			Assert.IsTrue((Guid)secondRow.Guid == Guid.Empty);
+			Console.WriteLine(tinyormVersion);
+		}
+	}//class
+}//ns

a6ddee33afefc0143d8d0649251eca37e8890093
Stan Drapkin <sdrapkin@sdprime.com>
2018-12-27 17:21:19 -0500

    Reflection improvements.

--- Utils/ReflectionHelper_Shared.cs
+++ Utils/ReflectionHelper_Shared.cs
@@ -7,7 +7,7 @@ namespace SecurityDriven.TinyORM.Utils
 {
 	public static class ReflectionHelper_Shared
 	{
-		public static readonly Type ObjectType = typeof(object);
+		static Type ObjectType = typeof(object);
 		public const BindingFlags propertyBindingFlags = (BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Instance);
 		public const string PARAM_PREFIX = "@";
 
@@ -38,7 +38,7 @@ namespace SecurityDriven.TinyORM.Utils
 			return dictionary;
 		}// ObjectToDictionary_Parameterized<T>
 
-		static ConstructorInfo _ValueTupleCtor = typeof(ValueTuple<object, Type>).GetConstructors()[0];
+		static ConstructorInfo s_ValueTupleCtor = typeof(ValueTuple<object, Type>).GetConstructors()[0];
 		internal static Dictionary<string, Func<T, (object, Type)>> GetPropertyGetters<T>(Type type, string prefix = "") where T : class
 		{
 			var source = type.ContainsGenericParameters ? Util.ZeroLengthArray<PropertyInfo>.Value : type.GetProperties(propertyBindingFlags);
@@ -46,18 +46,25 @@ namespace SecurityDriven.TinyORM.Utils
 
 			var pInstance = Expression.Parameter(type);
 			var parameterExpressionArray = new[] { pInstance };
+
 			var expressionArrayOf2 = new Expression[2];
+			ref var expressionArrayOf2_0 = ref expressionArrayOf2[0];
+			ref var expressionArrayOf2_1 = ref expressionArrayOf2[1];
+
+			ref var _ValueTupleCtor = ref s_ValueTupleCtor;
+			ref var _ObjectType = ref ObjectType;
+
 			foreach (PropertyInfo p in source)
 			{
-				if (p.GetIndexParameters().Length != 0) continue;
+				string pName = p.Name;
 				MethodInfo getMethod = p.GetGetMethod(nonPublic: true);
-				if (getMethod != null)
+				if ((getMethod != null) && (pName != "Item" || getMethod.GetParameters().Length == 0))
 				{
-					UnaryExpression body = Expression.Convert(Expression.Call(pInstance, getMethod), ObjectType);
-					expressionArrayOf2[0] = body;
-					expressionArrayOf2[1] = Expression.Constant(p.PropertyType);
+					UnaryExpression body = Expression.Convert(Expression.Call(pInstance, getMethod), _ObjectType);
+					expressionArrayOf2_0 = body;
+					expressionArrayOf2_1 = Expression.Constant(p.PropertyType);
 					var valueTupleBody = Expression.New(_ValueTupleCtor, expressionArrayOf2);
-					dictionary[prefix + p.Name] = Expression.Lambda<Func<T, (object, Type)>>(valueTupleBody, parameterExpressionArray).Compile();
+					dictionary[prefix + pName] = Expression.Lambda<Func<T, (object, Type)>>(valueTupleBody, parameterExpressionArray).Compile();
 				}
 			}//foreach PropertyInfo
 
@@ -69,7 +76,8 @@ namespace SecurityDriven.TinyORM.Utils
 			var source = type.ContainsGenericParameters ? Util.ZeroLengthArray<PropertyInfo>.Value : type.GetProperties(propertyBindingFlags);
 			var dictionary = new Dictionary<string, Action<T, object>>(source.Length, Util.FastStringComparer.Instance);
 
-			var valueCast_container = new UnaryExpression[1];
+			var valueCast_container = new Expression[1];
+			ref var valueCast_container_0 = ref valueCast_container[0];
 
 			var pInstance = Expression.Parameter(type, "instance");
 			var pValue = Expression.Parameter(ObjectType, "value");
@@ -77,15 +85,14 @@ namespace SecurityDriven.TinyORM.Utils
 
 			foreach (PropertyInfo p in source)
 			{
-				if (p.GetIndexParameters().Length != 0) continue;
+				string pName = p.Name;
 				MethodInfo setMethod = p.GetSetMethod(nonPublic: true);
-				if (setMethod != null)
+				if ((setMethod != null) && (pName != "Item" || setMethod.GetParameters().Length == 1))
 				{
-					var valueCast = Expression.Convert(pValue, p.PropertyType);
-					valueCast_container[0] = valueCast;
+					valueCast_container_0 = Expression.Convert(pValue, p.PropertyType);
 
 					var setter = Expression.Lambda<Action<T, object>>(Expression.Call(pInstance, setMethod, valueCast_container), instance_and_value_container).Compile();
-					dictionary[p.Name] = setter;
+					dictionary[pName] = setter;
 				}
 			}//foreach PropertyInfo
 

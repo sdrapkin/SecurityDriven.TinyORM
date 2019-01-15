8cee141686f09d74182f6327939b11c079056212
Stan Drapkin <sdrapkin@sdprime.com>
2019-01-15 11:17:31 -0500

    Version bump to 2.0.

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("SecurityDriven.TinyORM")]
-[assembly: AssemblyCopyright("Copyright © 2018 Stan Drapkin")]
+[assembly: AssemblyCopyright("Copyright © 2019 Stan Drapkin")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.2.*")]
+[assembly: AssemblyVersion("2.0.*")]
 [assembly: System.CLSCompliant(true)]

dcf0b18e6555089b8d9e64aeee732cfc52f50830
Stan Drapkin <sdrapkin@sdprime.com>
2020-02-13 15:05:31 -0500

    Revert "Going back to Expression-based object-factory (benchmarks showed it's faster)."
    This reverts commit 86e1aab3bddb15361aebfca6a4b6798ad3542677.

--- Utils/New.cs
+++ Utils/New.cs
@@ -1,10 +1,47 @@
 ﻿using System;
 using System.Linq.Expressions;
+using System.Reflection.Emit;
 
 namespace SecurityDriven.TinyORM.Utils
 {
 	internal static class New<T> where T : new() // very fast object/struct factory
 	{
-		public static readonly Func<T> Instance = Expression.Lambda<Func<T>>(Expression.New(typeof(T)), Util.ZeroLengthArray<ParameterExpression>.Value).Compile();
+		public static readonly Func<T> Instance = DynamicModuleLambdaCompiler.GenerateFactory<T>();
 	}//class New<T>
+
+	// https://blogs.msdn.microsoft.com/seteplia/2017/02/01/dissecting-the-new-constraint-in-c-a-perfect-example-of-a-leaky-abstraction/
+	static class DynamicModuleLambdaCompiler
+	{
+		static System.Reflection.Module _module = typeof(DynamicModuleLambdaCompiler).Module;
+		public static Func<T> GenerateFactory<T>() where T : new()
+		{
+			Expression<Func<T>> expr = () => new T();
+			NewExpression newExpr = (NewExpression)expr.Body;
+
+			var method = new DynamicMethod(
+				name: "lambda",
+				returnType: newExpr.Type,
+				parameterTypes: Util.ZeroLengthArray<Type>.Value,
+				m: _module,
+				skipVisibility: true);
+
+			ILGenerator ilGen = method.GetILGenerator();
+			// Constructor for value types could be null
+			if (newExpr.Constructor != null)
+			{
+				ilGen.Emit(OpCodes.Newobj, newExpr.Constructor);
+			}
+			else
+			{
+				LocalBuilder temp = ilGen.DeclareLocal(newExpr.Type);
+				ilGen.Emit(OpCodes.Ldloca, temp);
+				ilGen.Emit(OpCodes.Initobj, newExpr.Type);
+				ilGen.Emit(OpCodes.Ldloc, temp);
+			}
+
+			ilGen.Emit(OpCodes.Ret);
+
+			return (Func<T>)method.CreateDelegate(typeof(Func<T>));
+		}// GenerateFactory<T>()
+	}//class DynamicModuleLambdaCompiler
 }//ns
